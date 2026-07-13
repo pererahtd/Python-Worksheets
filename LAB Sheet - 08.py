f72970bc52f46f73c6520e216ea1d3915089e557
pererahtd <pererahtd@students.nsbm.ac.lk>
2026-07-13 14:46:49 +0530

    Add files via upload

---
+++ LAB Sheet - 08.py
@@ -0,0 +1,86 @@
+"""
+count=1
+tot=0
+tots=0
+while count<=5:
+    tot=tot+1
+    count=count+1
+    tots=tots+tot
+    print(tots)"""
+"""
+tot=0
+i=1
+while i<=5:
+    num=int(input(f"enter number {i}:"))
+    tot=tot+num
+    i=i+1
+print(f"total is {tot}")"""
+
+"""
+i=1
+even=0
+odd=0
+while i <=5:
+    num=int(input(f"enter number {i}:"))
+    if num % 2==0:
+        even=even+1
+    else:
+        odd=odd+1
+    i=i+1
+print(f"total even is {even}")
+print(f"total odd is {odd}")
+"""
+
+"""
+i=1
+
+while i<=3:
+    print("")
+    j=1
+    while j<=3:
+        print(j,end="")
+        j=j+1
+    i=i+1
+"""
+"""
+i=1
+
+while i<=4:
+    print("")
+    j=1
+    while j<=4:
+        print("*",end="")
+        j=j+1
+    i=i+1
+    """
+
+"""
+i=1
+
+while i<=4:
+    print("")
+    j=1
+    while j<=i:
+        print(j,end="")
+        j=j+1
+    i=i+1
+"""
+
+i=1
+
+while i<=5:
+    print("")
+    j=i
+    while j<=5:
+        print("*",end="")
+        j=j+1
+    i=i+1
+
+
+
+
+
+
+
+        
+    
